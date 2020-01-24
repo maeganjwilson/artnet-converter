c02b585ebb944a8ab70cca381e6a368794e52aa1
Maegan Wilson <maegan@maeganwilson.com>
2020-01-24 14:37:17 -0600

    Changed formatting and working on assuming 0 based

--- uni-converter/uni-converter/Views/Buttons/NumberButton.swift
+++ uni-converter/uni-converter/Views/Buttons/NumberButton.swift
@@ -9,32 +9,32 @@
 import SwiftUI
 
 struct NumberButton: View {
-    @EnvironmentObject var calcManager: CalculationManager
-    @EnvironmentObject var settingsManager: SettingsManager
-    
-    @Binding var toggle: Bool
-    var num: Int
-    let frameSize: CGFloat = 80.0
-    
-    var body: some View {
-        Button(action: {
-            if self.toggle == true {
-                self.toggle = false
-                self.calcManager.setPimUni(to: 0)
-            }
-            self.calcManager.addNumberToPrimUni(self.num)
-        }){
-            Text("\(self.num)")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-                .frame(width: frameSize, height: frameSize)
-                .background(Color.gray)
-                .cornerRadius(2.0)
-                .padding(.all, 10.0)
-                .shadow(radius: 2.0)
-        }
+  @EnvironmentObject var calcManager: CalculationManager
+  @EnvironmentObject var settingsManager: SettingsManager
+  
+  @Binding var toggle: Bool
+  var num: Int
+  let frameSize: CGFloat = 80.0
+  
+  var body: some View {
+    Button(action: {
+      if self.toggle == true {
+        self.toggle = false
+        self.calcManager.setPimUni(to: 0)
+      }
+      self.calcManager.addNumberToPrimUni(self.num)
+    }){
+      Text("\(self.num)")
+        .font(.title)
+        .fontWeight(.bold)
+        .foregroundColor(.white)
+        .frame(width: frameSize, height: frameSize)
+        .background(Color.gray)
+        .cornerRadius(2.0)
+        .padding(.all, 10.0)
+        .shadow(radius: 2.0)
     }
+  }
 }
 
 //struct NumberButton_Previews: PreviewProvider {
